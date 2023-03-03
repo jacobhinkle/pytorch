df7303d111eba15b56d9c055908b6542a1240e50
Jacob Hinkle <jhinkle@nvidia.com>
2023-03-03 05:48:02 -0800

    Refactor, add Relations

--- third_party/nvfuser/csrc/id_e_graph.cpp
+++ third_party/nvfuser/csrc/id_e_graph.cpp
@@ -9,137 +9,22 @@ namespace jit {
 namespace fuser {
 namespace cuda {
 
-// A fixed-size tree-based union-find (aka disjoint-set) data structure using
-// subtree sizes instead of ranks.
-// cf. https://en.wikipedia.org/wiki/Disjoint-set_data_structure
-template <typename T>
-class UnionFind {
- public:
-  UnionFind(size_t size) {
-      value_.resize(size);
-      parent_.resize(size);
-      size_.resize(size);
-      // Initialize with all singletoons
-      for (size_t i = 0; i < size; ++i) {
-        parent_[i] = i;
-        size_[i] = 1;
-      }
-  }
-
-  UnionFind(std::vector<T> vals) : UnionFind(vals.size()) {
-    for (size_t i = 0; i < vals.size(); ++i) {
-      this->set_value(i, vals[i]);
-    }
-  }
-
-  UnionFind(std::unordered_set<T> vals) : UnionFind(vals.size()) {
-    size_t i = 0;
-    for (auto v: vals) {
-      this->set_value(i++, v);
-    }
-  }
-
-  void set_value(int pos, const T& val) {
-    value_[pos] = val;
-    val_to_pos_[val] = pos;
-  }
-  T get_value(int pos) {
-    TORCH_CHECK(
-      pos < value_.size(),
-      "Passed invalid position ", pos, " for UnionFind with ", value_.size(), " entries"
-    );
-    return value_[pos];
-  }
-
-  //! Insert the given value and return the new number of elements
-  size_t insert_value(const T& val) {
-    auto pos = parent_.size();
-    parent_.push_back(pos);
-    size_.push_back(1);
-    val_to_pos_[val] = pos;
-    value_.push_back(val);
-    return pos + 1;
-  }
-
-  //! Find the integer position of val
-  size_t get_position(const T& val) {
-    return val_to_pos_.at(val);
-  }
-
-  //! Get the integer index of the set from given position
-  size_t find_set(size_t v) {
-    if (v == parent_[v])
-      return v;
-    // Note that this step actually updates the tree to point directly to the
-    // root index, meaning subsequent look-ups will not need to recurse.
-    return parent_[v] = find_set(parent_[v]);
-  }
-  //! Get the integer index of the set for a given value
-  size_t find_set_from_value(T val) {
-    return find_set(get_position(val));
-  }
-
-  //! Get all elements in the set with given index (up to O(n^2))
-  std::vector<T> get_set(size_t idx) {
-    std::vector<T> s;
-    for (size_t i = 0; i < parent_.size(); ++i) {
-      if (find_set(i) == idx) {
-        s.push_back(value_.at(i));
-      }
-    }
-    return s;
-  }
-
-  //! Get a vector of all sets of values
-  std::vector<std::vector<T>> get_sets() {
-    std::vector<std::vector<T> > out;
-    for (size_t i = 0; i < parent_.size(); ++i) {
-      auto s = get_set(i);
-      if (s.size() > 0) {
-        out.push_back(s);
-      }
-    }
-    return out;
-  }
-
-  //! Merge two sets in the partition
-  void merge_sets(size_t a, size_t b) {
-    if (a != b) {
-      if (size_[a] < size_[b])
-        std::swap(a, b);
-      parent_[b] = a;
-      size_[a] += size_[b];
-    }
-  }
-  //! Merge the sets containing two given values
-  void merge_sets_from_values(T val_a, T val_b) {
-    auto a = find_set(get_position(val_a));
-    auto b = find_set(get_position(val_b));
-    merge_sets(a, b);
-  }
- private:
-  std::vector<T> value_;
-  std::unordered_map<T, size_t> val_to_pos_;
-  std::vector<size_t> parent_;
-  std::vector<int> size_;
-};
-
 IterDomainEGraph::IterDomainEGraph(const Fusion& fusion) {
   // Initialize a partition of all IterDomains in the Fusion, initially
   // containing all singleton classes.
-  std::vector<IterDomain*> all_ids;
-  std::unordered_set<Val*> all_extents; // Also track extents to find which are equivalent
-  for (auto v: fusion.vals()) {
+  for (auto v : fusion.vals()) {
     if (v->getValType() == ValType::IterDomain) {
       auto id = reinterpret_cast<IterDomain*>(v);
-      all_ids.push_back(id);
-      all_extents.insert(id->extent());
+      all_ids_.push_back(id);
+      all_extents_.insert(id->extent());
     }
   }
-  UnionFind<IterDomain*> id_partition(all_ids);
-  UnionFind<Val*> extent_partition(all_extents);
+  id_partition_ =
+      std::unique_ptr<UnionFind<IterDomain*>>(new UnionFind(all_ids_));
+  extent_partition_ =
+      std::unique_ptr<UnionFind<Val*>>(new UnionFind(all_extents_));
 
-  for (auto expr: fusion.unordered_exprs()) {
+  for (auto expr : fusion.unordered_exprs()) {
     // Expressions fall into one of the following categories:
     //   Broadcast
     //   Reduction
@@ -161,8 +46,9 @@ IterDomainEGraph::IterDomainEGraph(const Fusion& fusion) {
         }
         auto idin = indom[j++];
         auto idout = outdom[i];
-        id_partition.merge_sets_from_values(idin, idout);
-        extent_partition.merge_sets_from_values(idin->extent(), idout->extent());
+        id_partition_->merge_sets_from_values(idin, idout);
+        extent_partition_->merge_sets_from_values(
+            idin->extent(), idout->extent());
       }
     } else if (expr->isA<ReductionOp>()) {
       std::cout << "Reduction op: " << expr->toString() << std::endl;
@@ -185,8 +71,9 @@ IterDomainEGraph::IterDomainEGraph(const Fusion& fusion) {
           // TODO: set RESOLVES relation
           continue;
         }
-        id_partition.merge_sets_from_values(idin, idout);
-        extent_partition.merge_sets_from_values(idin->extent(), idout->extent());
+        id_partition_->merge_sets_from_values(idin, idout);
+        extent_partition_->merge_sets_from_values(
+            idin->extent(), idout->extent());
       }
     } else if (expr->isA<TransposeOp>()) {
       auto top = reinterpret_cast<TransposeOp*>(expr);
@@ -199,13 +86,14 @@ IterDomainEGraph::IterDomainEGraph(const Fusion& fusion) {
         auto oldi = n2o[i];
         auto idin = indom[oldi];
         auto idout = outdom[i];
-        id_partition.merge_sets_from_values(idin, idout);
-        extent_partition.merge_sets_from_values(idin->extent(), idout->extent());
+        id_partition_->merge_sets_from_values(idin, idout);
+        extent_partition_->merge_sets_from_values(
+            idin->extent(), idout->extent());
       }
     } else if (expr->isA<ViewOp>()) {
       std::cout << "Skipping reshape op: " << expr->toString() << std::endl;
-    //} else if (expr->isA<ResizeOp>()) { // pending Naoya's recent work
-    // TODO: Work through the list of other ops:
+      //} else if (expr->isA<ResizeOp>()) { // pending Naoya's recent work
+      // TODO: Work through the list of other ops:
       /*
      *FullOp # nothing to do
      *ARangeOp # nothing to do
@@ -245,54 +133,53 @@ IterDomainEGraph::IterDomainEGraph(const Fusion& fusion) {
       //   the broadcast), then we do not merge.
       //   Instead, in this case we add a relation that the e-class of the
       //   output ID _resolves_ the e-class of the input ID.
-      for (auto inp_val: expr->inputs()) {
+      for (auto inp_val : expr->inputs()) {
         if (inp_val->getValType() != ValType::TensorView) {
           continue;
         }
         auto inp = (TensorView*)inp_val;
         auto indom = inp->domain()->noReductions();
-        for (auto outp_val: expr->outputs()) {
+        for (auto outp_val : expr->outputs()) {
           if (outp_val->getValType() != ValType::TensorView) {
             continue;
           }
           auto outp = (TensorView*)outp_val;
-          // Reduction domains aren't preserved through pointwise ops, so ignore them
-          // For each non-reduction ID in inp and outp
+          // Reduction domains aren't preserved through pointwise ops, so ignore
+          // them For each non-reduction ID in inp and outp
           auto outdom = outp->domain()->noReductions();
           TORCH_CHECK(
               indom.size() == outdom.size(),
-              "Input and output noReductions domains must have equal length in pointwise op"
-          );
-          for (auto idin=indom.begin(), idout=outdom.begin();
-              idin != indom.end() && idout != outdom.end();
-              idin++, idout++) {
+              "Input and output noReductions domains must have equal length in pointwise op");
+          for (auto idin = indom.begin(), idout = outdom.begin();
+               idin != indom.end() && idout != outdom.end();
+               idin++, idout++) {
             TORCH_CHECK(
-              !(*idout)->isBroadcast(),
-              "Output IterDomains of pointwise ops should not be of broadcast type"
-            );
+                !(*idout)->isBroadcast(),
+                "Output IterDomains of pointwise ops should not be of broadcast type");
             // TODO: check for broadcast IDs in input
             if ((*idin)->isBroadcast()) {
               continue;
             }
-            id_partition.merge_sets_from_values(*idin, *idout);
-            extent_partition.merge_sets_from_values((*idin)->extent(), (*idout)->extent());
+            id_partition_->merge_sets_from_values(*idin, *idout);
+            extent_partition_->merge_sets_from_values(
+                (*idin)->extent(), (*idout)->extent());
           }
         }
       }
     }
   }
   std::cout << "Equivalence classes of IterDomains:" << std::endl;
-  for (auto s: id_partition.get_sets()) {
+  for (auto s : id_partition_->get_sets()) {
     std::cout << "  ";
-    for (auto id: s) {
+    for (auto id : s) {
       std::cout << id->toString() << ", ";
     }
     std::cout << std::endl;
   }
   std::cout << "Equivalence classes of extents:" << std::endl;
-  for (auto s: extent_partition.get_sets()) {
+  for (auto s : extent_partition_->get_sets()) {
     std::cout << "  ";
-    for (auto e: s) {
+    for (auto e : s) {
       std::cout << e->toString() << ", ";
     }
     std::cout << std::endl;
--- third_party/nvfuser/csrc/id_e_graph.h
+++ third_party/nvfuser/csrc/id_e_graph.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ir_all_nodes.h>
+#include <union_find.h>
 
 #include <memory>
 #include <vector>
@@ -18,21 +19,70 @@ class TORCH_CUDA_CU_API IterDomainENode {
   std::weak_ptr<IterDomain> iter_domain_;
 };
 
-//! Implements an equivalence class of IterDomains.
-class TORCH_CUDA_CU_API IterDomainEClass {
- private:
-  std::vector<IterDomainENode> e_nodes_;
+enum RelationType {
+  ResolvesBroadcast,
+  Reduces,
+  InnerToInInput,
+  InnerToInOutput
+};
+
+//! Abstract class for binary relations
+class Relation {
+ public:
+  Relation(size_t left_id, size_t right_id)
+      : left_id_(left_id), right_id_(right_id){};
+  virtual ~Relation() {}
+  virtual RelationType type() const = 0;
+
+ protected:
+  size_t left_id_, right_id_;
+};
+
+//! Left domain is inner to right domain in at least one input TensorView
+class InnerToInInputRelation : Relation {
+  RelationType type() const {
+    return RelationType::InnerToInInput;
+  }
+};
+
+//! Left domain is inner to right domain in at least one output TensorView
+class InnerToInOutputRelation : Relation {
+  RelationType type() const {
+    return RelationType::InnerToInOutput;
+  }
+};
+
+//! Left domain resolves the right (broadcast) domain
+class ResolvesBroadcastRelation : Relation {
+  RelationType type() const {
+    return RelationType::ResolvesBroadcast;
+  }
+};
+
+//! Left domain is a reduction domain over right domain
+class ReducesRelation : Relation {
+  RelationType type() const {
+    return RelationType::Reduces;
+  }
 };
 
 //! Implements an E-graph whose "terms" are IterDomains.
 class TORCH_CUDA_CU_API IterDomainEGraph {
  public:
-   IterDomainEGraph(const Fusion& fusion);
+  IterDomainEGraph(const Fusion& fusion);
 
+  //! Merge the sets representing two values, preserving relations from each
+  void merge_sets_from_values(const Val* a, const Val* b);
 
  private:
-   Fusion* fusion;
-   std::vector<IterDomainEClass> e_classes_;
+  Fusion* fusion;
+  std::vector<int> e_class_ids_;
+  std::vector<Relation*> relations_;
+  std::vector<IterDomain*> all_ids_;
+  std::unordered_set<Val*>
+      all_extents_; // Also track extents to find which are equivalent
+  std::unique_ptr<UnionFind<IterDomain*>> id_partition_;
+  std::unique_ptr<UnionFind<Val*>> extent_partition_;
 };
 
 } // namespace cuda
