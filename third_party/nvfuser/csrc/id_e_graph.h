c89d81817255a041e4b65fe908c6d7c53bbcadee
Jacob Hinkle <jhinkle@nvidia.com>
2023-03-03 09:49:52 -0800

    Abstract relations over type of nodes

--- third_party/nvfuser/csrc/id_e_graph.h
+++ third_party/nvfuser/csrc/id_e_graph.h
@@ -3,7 +3,9 @@
 #include <ir_all_nodes.h>
 #include <union_find.h>
 
+#include <iostream>
 #include <memory>
+#include <unordered_set>
 #include <vector>
 
 namespace torch {
@@ -27,40 +29,44 @@ enum RelationType {
 };
 
 //! Abstract class for binary relations
+template <typename T>
 class Relation {
  public:
-  Relation(size_t left_id, size_t right_id)
-      : left_id_(left_id), right_id_(right_id){};
+  Relation(T left_id, T right_id) : left_id_(left_id), right_id_(right_id){};
   virtual ~Relation() {}
   virtual RelationType type() const = 0;
 
  protected:
-  size_t left_id_, right_id_;
+  T left_id_, right_id_;
 };
 
 //! Left domain is inner to right domain in at least one input TensorView
-class InnerToInInputRelation : Relation {
+template <typename T>
+class InnerToInInputRelation : Relation<T> {
   RelationType type() const {
     return RelationType::InnerToInInput;
   }
 };
 
 //! Left domain is inner to right domain in at least one output TensorView
-class InnerToInOutputRelation : Relation {
+template <typename T>
+class InnerToInOutputRelation : Relation<T> {
   RelationType type() const {
     return RelationType::InnerToInOutput;
   }
 };
 
 //! Left domain resolves the right (broadcast) domain
-class ResolvesBroadcastRelation : Relation {
+template <typename T>
+class ResolvesBroadcastRelation : Relation<T> {
   RelationType type() const {
     return RelationType::ResolvesBroadcast;
   }
 };
 
 //! Left domain is a reduction domain over right domain
-class ReducesRelation : Relation {
+template <typename T>
+class ReducesRelation : Relation<T> {
   RelationType type() const {
     return RelationType::Reduces;
   }
@@ -71,13 +77,24 @@ class TORCH_CUDA_CU_API IterDomainEGraph {
  public:
   IterDomainEGraph(const Fusion& fusion);
 
-  //! Merge the sets representing two values, preserving relations from each
-  void merge_sets_from_values(const Val* a, const Val* b);
+  //! Get all class IDs, which are not guaranteed to be a contiguous list of
+  //! size_t's
+  std::vector<size_t> getClassIds();
+
+  //! Convert internal representation to relations over _classes_
+  std::vector<Relation<size_t>*> getClassRelations();
+
+  //! Print out a diagram in mermaid format
+  //! https://mermaid.js.org
+  //! https://mermaid.live
+  void printMermaid(std::ostream& stream) {
+    stream << "graph TD" << std::endl;
+  }
 
  private:
   Fusion* fusion;
   std::vector<int> e_class_ids_;
-  std::vector<Relation*> relations_;
+  std::vector<Relation<IterDomain*>*> id_relations_;
   std::vector<IterDomain*> all_ids_;
   std::unordered_set<Val*>
       all_extents_; // Also track extents to find which are equivalent
